c2b9b517c08126bd234641f61ab4f3f274a215f9
emirhansal <76394207+emirhansal@users.noreply.github.com>
2022-04-15 04:24:01 +0300

    add bike button done
    add bike button done

--- src/kelebike/lib/screens/admin_add_bike_page.dart
+++ src/kelebike/lib/screens/admin_add_bike_page.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
+import 'package:kelebike/screens/admin_bikes_screen.dart';
 import 'package:kelebike/service/auth.dart';
+import 'package:kelebike/service/bike_service.dart';
 import 'package:kelebike/utilities/constants.dart';
 
 class adminAddBikePage extends StatefulWidget {
@@ -10,6 +13,165 @@ class adminAddBikePage extends StatefulWidget {
 }
 
 class _adminAddBikePageState extends State<adminAddBikePage> {
+  var _SerialNoController = TextEditingController();
+  var _BrandController = TextEditingController();
+  var _CodeController = TextEditingController();
+  var _StatusController = TextEditingController();
+
+  BikeService _bikeService = BikeService();
+  User? _user = FirebaseAuth.instance.currentUser;
+
+  Widget _buildSerialNo() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Serial No:',
+          style: kLabelStyle,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyle,
+          height: 60.0,
+          child: TextField(
+            controller: _SerialNoController,
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.circle,
+                color: Colors.white,
+              ),
+              hintText: 'Serial No',
+              hintStyle: kHintTextStyle,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _Brand() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Brand:',
+          style: kLabelStyle,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyle,
+          height: 60.0,
+          child: TextField(
+            controller: _BrandController,
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.circle,
+                color: Colors.white,
+              ),
+              hintText: 'Brand',
+              hintStyle: kHintTextStyle,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _Code() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Code:',
+          style: kLabelStyle,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyle,
+          height: 60.0,
+          child: TextField(
+            controller: _CodeController,
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'OpenSans',
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.circle,
+                color: Colors.white,
+              ),
+              hintText: 'Code',
+              hintStyle: kHintTextStyle,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildAddBike() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5,
+        onPressed: () async {
+          _bikeService.addBike(
+              serialNumber: _SerialNoController.text,
+              brand: _BrandController.text,
+              code: _CodeController.text,
+              status: 'nontaken',
+              dateIssued: 'nontaken',
+              dateReturn: 'nontaken',
+              owner: 'nontaken');
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => AdminBikesScreen()));
+        },
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        color: Colors.white,
+        child: Text(
+          'Add Bike',
+          style: TextStyle(
+            color: Color(0xFF527DAA),
+            letterSpacing: 1.5,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'OpenSans',
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,36 +179,49 @@ class _adminAddBikePageState extends State<adminAddBikePage> {
       appBar: AppBar(
         backgroundColor: Colors.lightBlueAccent,
         elevation: 0,
-        title: Text("Add a Bike"),
+        title: Text("Add Bike"),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: [
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Stack(
+            children: <Widget>[
               Container(
-                  child: Stack(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 50, left: 20),
-                    child: Text(
-                      "burada bisiklet kiralama",
-                      style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(255, 39, 37, 37)),
-                    ),
+                  height: double.infinity,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF6CA8F1),
+                  )),
+              Container(
+                height: double.infinity,
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40.0,
+                    vertical: 40.0,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 90, left: 20),
-                    child: Text(
-                      "geçmişi bulunur!",
-                      style: TextStyle(
-                          fontSize: 30, color: Color.fromARGB(255, 22, 14, 14)),
-                    ),
-                  )
-                ],
-              )),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      _buildSerialNo(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      _Brand(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      _Code(),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      _buildAddBike()
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
