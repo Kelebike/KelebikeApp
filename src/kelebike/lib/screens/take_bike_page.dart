63dc1cf62078e5657ce12d7e6d04f85c6237a23e
baygut <berkaybaygut@gmail.com>
2022-05-19 21:40:36 +0300

    qr scanner added to take bike page
    oldu lan!

--- src/kelebike/lib/screens/take_bike_page.dart
+++ src/kelebike/lib/screens/take_bike_page.dart
@@ -3,9 +3,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:kelebike/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:kelebike/screens/qr_overlay.dart';
+import 'package:kelebike/screens/qr_scanner_controller.dart';
 import 'package:kelebike/service/auth.dart';
 import 'package:kelebike/service/bike_service.dart';
 import 'package:kelebike/utilities/constants.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 class TakeBikePage extends StatefulWidget {
   @override
@@ -50,6 +53,8 @@ class _TakeBikePageState extends State<TakeBikePage> {
   BikeService _bikeService = BikeService();
   User? _user = FirebaseAuth.instance.currentUser;
 
+  MobileScannerController cameraController = MobileScannerController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,52 +63,39 @@ class _TakeBikePageState extends State<TakeBikePage> {
         backgroundColor: Color(0xFF6CA8F1),
         elevation: 0,
         title: Text("Take a bike"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                cameraController.switchCamera();
+              },
+              icon: const Icon(Icons.camera_rear_outlined))
+        ],
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: [
-              Container(
-                  child: Stack(
-                children: [
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(top: 100, left: 20, right: 20),
-                    child: _buildBikeCode(),
-                  ),
-                ],
-              )),
-              SizedBox(height: 20),
-              ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all(Color(0xFF6F35A5)),
-                    padding: MaterialStateProperty.all(EdgeInsets.all(20)),
-                    textStyle:
-                        MaterialStateProperty.all(TextStyle(fontSize: 15))),
-                onPressed: () async {
-                  bool a = await _bikeService.takeBike(
-                      _bikeCodeController.text, _user!.email.toString());
+      body: Stack(
+        children: [
+          MobileScanner(
+            allowDuplicates: false,
+            controller: cameraController,
+            onDetect: (barcode, args) async {
+              String? _barcode = barcode.rawValue;
+              debugPrint('Barcode Found!' + barcode.rawValue!);
 
-                  if (await _bikeService
-                          .findWithBikeCode(_bikeCodeController.text) ==
-                      null) {
-                    showDialog(
-                        context: context,
-                        builder: (_) => AlertDialog(
-                              title: Text('Error'),
-                              content: Text('Bike not found!'),
-                            ));
-                  } else {
-                    Navigator.pop(context);
-                  }
-                },
-                child: Text("Send request"),
-              )
-            ],
+              if (await _bikeService.findWithBikeCode(_barcode!) == null) {
+                showDialog(
+                    context: context,
+                    builder: (_) => AlertDialog(
+                          title: Text('Error'),
+                          content: Text('Bike not found!'),
+                        ));
+              } else {
+                _bikeService.takeBike(_barcode, _user!.email.toString());
+                Navigator.pop(context);
+              }
+            },
           ),
-        ),
+          QRScannerOverlay(overlayColour: Colors.black.withOpacity(0.5))
+        ],
       ),
     );
   }
